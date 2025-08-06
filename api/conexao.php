2630824c3d7a3a6ce40b80648908dba775d0a6ae
Gabriel Souza <gcbs109@gmail.com>
2025-08-06 17:52:42 -0300

    att10

--- api/conexao.php
+++ api/conexao.php
@@ -1,20 +1,12 @@
 <?php
-$host = "db.iavurvzpfgpihsfadmmx.supabase.co"; // ex: db.xxxxxxxxx.supabase.co
-$port = "5432";
-$dbname = "postgres"; // Nome padrão do banco
-$user = "postgres";   // Usuário padrão
-$password = "Ga29164476";
+$host = 'localhost';
+$dbname = 'dbimoveis';
+$user = 'root';
+$pass = '';
 
-// String de conexão
-$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require";
-
-// Conectar
-$conn = pg_connect($conn_string);
-
-// Verificar conexão
-if (!$conn) {
-    die("Erro ao conectar ao banco de dados PostgreSQL no Supabase.");
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
 }
-
-// echo "Conexão bem-sucedida!";
 ?>
